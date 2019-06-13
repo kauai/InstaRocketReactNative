6107a3baface86760865a38d3d23629c4bfdfdfe
Thiago kauai <html5ephp@gmail.com>
2019-06-13 19:47:31 -0300

    Buscando dados da api,Listando conteudo,design implementado

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import Routes from './routes'
+
+export default function App() {
+    return <Routes/>
+}
